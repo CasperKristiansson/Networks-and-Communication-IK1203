320a5e9dbf543f8e293e9d3771b4227a8e441eda
Casper Kristiansson <casper.kristiansson@yahoo.se>
2022-02-14 07:50:24 +0100

    Project Taks 2 Done

--- Project Socket Programming/task2/tcpclient/TCPClient.java
+++ Project Socket Programming/task2/tcpclient/TCPClient.java
@@ -14,30 +14,41 @@ public class TCPClient {
         this.limit = limit;
     }
 
-    public byte[] askServer(String hostname, int port, byte[] bytesToServer) throws IOException {       
+    public byte[] askServer(String hostname, int port, byte[] bytesToServer) throws IOException {  
+        System.out.println("Shutdown: " + shutdown + " Timeout: " + timeout + " Limit: " + limit);     
         Socket socket = new Socket(hostname, port);
-        socket.getOutputStream().write(bytesToServer);
 
-        byte[] buffer = new byte[BUFFERSIZE];
-        
+        socket.getOutputStream().write(bytesToServer);
+        if (shutdown) socket.shutdownOutput();
+
         InputStream inputStream = socket.getInputStream();
         ByteArrayOutputStream result = new ByteArrayOutputStream();
+        byte[] buffer = new byte[BUFFERSIZE];
         int readBytes;
 
-        if (this.timeout != null) socket.setSoTimeout(this.timeout);
+        try {
+            // Sets a maximum time to wait for data
+            if (this.timeout != null) socket.setSoTimeout(this.timeout);
 
-        if (this.limit != null) {
-            int totalBytes = 0;
-            while ((readBytes = inputStream.read(buffer)) != -1) {
-                result.write(buffer, 0, readBytes);
-                totalBytes += readBytes;
+            if (this.limit != null) {
+                int totalBytes = 0;
+                
+                // If the limit is less than the buffer size, use the limit as the buffer size
+                if (this.limit < BUFFERSIZE) buffer = new byte[this.limit];
 
-                if (totalBytes >= this.limit) break;
+                while ((readBytes = inputStream.read(buffer)) != -1 && totalBytes < this.limit) {
+                    result.write(buffer, 0, readBytes);
+                    totalBytes += readBytes;
+
+                    if (totalBytes >= this.limit) break;
+                    // To get the exact amount of bytes we need to adjust the buffer size for the last read
+                    if (totalBytes + BUFFERSIZE > this.limit) buffer = new byte[this.limit - totalBytes];
+                }
+            } else {
+                while ((readBytes = inputStream.read(buffer)) != -1) result.write(buffer, 0, readBytes);
             }
-        }
-
-        else {
-            while ((readBytes = inputStream.read(buffer)) != -1) result.write(buffer, 0, readBytes);
+        } catch (SocketTimeoutException e) {
+            System.out.println("Socket timed out!");
         }
 
         if (this.shutdown) socket.close();
