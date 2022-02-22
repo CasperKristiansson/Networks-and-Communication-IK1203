7e5f3797e0a72c6b5136451d0104be5bb53cfc48
Casper Kristiansson <casper.kristiansson@yahoo.se>
2022-02-22 21:11:33 +0100

    Fix project task three when entering only subdomain

--- Project Socket Programming/task3/HTTPAsk.java
+++ Project Socket Programming/task3/HTTPAsk.java
@@ -10,75 +10,90 @@ public class HTTPAsk {
 
             while (true) {
                 Socket socket = serverSocket.accept();
-                // System.out.println("Accepted connection from " + socket.getInetAddress());
-                
-                byte[] buffer = new byte[1024];
-                socket.getInputStream().read(buffer);
-                String decodedString = new String(buffer, StandardCharsets.UTF_8);
-
                 OutputStream socketOutput = socket.getOutputStream();
-
                 StringBuilder sb = new StringBuilder();
-                String url = decodedString.split(" ")[1];
-                String[] params = url.split("\\?");
-               
-                if (params.length > 0 && params[0].equals("/ask")) {
-                    String[] paramList = params[1].split("&");
+                // System.out.println("Accepted connection from " + socket.getInetAddress());
 
-                    String hostname = "";
-                    int portClient = 0;
-                    byte[] bytesToServer = new byte[0];
-                    Integer timeout = null;
-                    Integer limit = null;
-                    boolean shutdown = false;
-                    
-                    for (String param : paramList) {
-                        String[] keyValue = param.split("=");
+                try {
+                    byte[] buffer = new byte[1024];
+                    socket.getInputStream().read(buffer);
+                    String decodedString = new String(buffer, StandardCharsets.UTF_8);
 
-                        if (keyValue[0].equals("shutdown")) {
-                            shutdown = Boolean.parseBoolean(keyValue[1]);
-                        } else if (keyValue[0].equals("timeout")) {
-                            timeout = Integer.parseInt(keyValue[1]);
-                        } else if (keyValue[0].equals("limit")) {
-                            limit = Integer.parseInt(keyValue[1]);
-                        } else if (keyValue[0].equals("hostname")) {
-                            hostname = keyValue[1];
-                        } else if (keyValue[0].equals("port")) {
-                            portClient = Integer.parseInt(keyValue[1]);
-                        } else if (keyValue[0].equals("string")) {
-                            bytesToServer = keyValue[1].getBytes();
+                    String url = decodedString.split(" ")[1];
+                    String[] params = url.split("\\?");
+                
+                    if (params.length > 0 && params[0].equals("/ask")) {
+                        if (params.length < 2) {
+                            sb.append("HTTP/1.1 400 Bad Request\r\n");
+                            throw new Exception("Bad Request");
                         }
-                    }
 
-                    if (hostname.equals("") || portClient == 0) {
-                        sb.append("HTTP/1.1 400 Bad Request\r\n");
+                        String[] paramList = params[1].split("&");
+                        // for(String param : paramList) System.out.println(param);
 
-                    } else {
+                        String hostname = "";
+                        int portClient = 0;
+                        byte[] bytesToServer = new byte[0];
+                        Integer timeout = null;
+                        Integer limit = null;
+                        boolean shutdown = false;
+                        
+                        for (String param : paramList) {
+                            String[] keyValue = param.split("=");
+
+                            if (keyValue[0].equals("shutdown")) {
+                                shutdown = Boolean.parseBoolean(keyValue[1]);
+                            } else if (keyValue[0].equals("timeout")) {
+                                timeout = Integer.parseInt(keyValue[1]);
+                            } else if (keyValue[0].equals("limit")) {
+                                limit = Integer.parseInt(keyValue[1]);
+                            } else if (keyValue[0].equals("hostname")) {
+                                hostname = keyValue[1];
+                            } else if (keyValue[0].equals("port")) {
+                                portClient = Integer.parseInt(keyValue[1]);
+                            } else if (keyValue[0].equals("string")) {
+                                bytesToServer = keyValue[1].getBytes();
+                            }
+                        }
+
+                        if (hostname.equals("") || portClient == 0) {
+                            sb.append("HTTP/1.1 400 Bad Request\r\n");
+                            throw new Exception("Bad Request");
+                        }
+
                         try {
                             sb.append("HTTP/1.1 200 OK\r\n\r\n");
 
                             TCPClient client = new TCPClient(shutdown, timeout, limit);
                             sb.append(new String(client.askServer(hostname, portClient, bytesToServer)));
 
-                        } catch (IOException e) {
+                        } catch (Exception e) {
                             sb.append("HTTP/1.1 500 Internal Server Error\r\n");
+                            throw new Exception("Internal Server Error");
                         }
+                        
+                    } else {
+                        sb.append("HTTP/1.1 404 Not Found\r\n");
+                        throw new Exception("Not Found");
                     }
-                    
-                } else {
-                    sb.append("HTTP/1.1 404 Not Found\r\n\r\n");
-                }
 
-                socketOutput.write(sb.toString().getBytes());
-                socket.close();
+                    socketOutput.write(sb.toString().getBytes());
+                    socket.close();
+                    // System.out.println(sb.toString());
+                    
+                } catch (Exception e) {
+                    socketOutput.write(sb.toString().getBytes());
+                    socket.close();
+                    // System.out.println(e + " " + sb.toString());
+                }
             }
 
         } catch (ArrayIndexOutOfBoundsException e) {
-            // System.out.println("Usage: java HTTPAsk <port>");
+            // System.out.println(e);
             System.exit(1);
 
         } catch (IOException e) {
-            // System.out.println("Could not listen on port);
+            // System.out.println(e);
             System.exit(1);
         }
     }
